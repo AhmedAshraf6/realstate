cbdf463113a38e750cb88869ef4ba5dbe26c3af0
ahmed <ahmedp76b@gmail.com>
2023-11-05 10:13:14 -0800

    coonect databse

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-
 const app = express();
+const connectDB = require('./db/connectDB');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.listen(5000, () => {
-  console.log('Server is running on port 5000!!!');
-});
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`listen ahmed to port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
