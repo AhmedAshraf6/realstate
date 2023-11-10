01077b9d636cc6af345e19c8da661c9192f2b98d
ahmed <ahmedp76b@gmail.com>
2023-11-09 19:17:24 -0800

    last2

--- client/src/app/GlobalRedux/ReduxProvider.jsx
+++ client/src/app/GlobalRedux/ReduxProvider.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './Store';
 
 export function ReduxProvider({ children }) {
   return <Provider store={store}>{children}</Provider>;
