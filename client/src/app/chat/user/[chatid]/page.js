88bb52139451a8d9476976d8270943976dce9e46
ahmed <ahmedp76b@gmail.com>
2023-11-22 06:54:31 -0800

    last updates

--- client/src/app/chat/user/[chatid]/page.js
+++ client/src/app/chat/user/[chatid]/page.js
@@ -1,9 +1,9 @@
 'use client';
 
 import {
-  BodySelectedChat,
-  HeaderSelectedChat,
-  InputSelectedChat,
+  // BodySelectedChat,
+  // HeaderSelectedChat,
+  // InputSelectedChat,
   UserChats,
 } from '@/components';
 
@@ -20,11 +20,11 @@ export default function Chat({ params }) {
   return (
     <div className='align-element grid grid-cols-3 h-[calc(100vh-64px)]'>
       <UserChats />
-      <div className='sm:col-span-2 rounded-lg border-2  flex flex-col h-[calc(100vh-64px)]'>
+      {/* <div className='sm:col-span-2 rounded-lg border-2  flex flex-col h-[calc(100vh-64px)]'>
         <HeaderSelectedChat />
         <BodySelectedChat chatId={params.chatid} />
         <InputSelectedChat chatId={params.chatid} />
-      </div>
+      </div> */}
     </div>
   );
 }
