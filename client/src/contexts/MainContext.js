331641090b12a0d13d6f672ddf02a4a7bfedf8e2
ahmed <ahmedp76b@gmail.com>
2023-11-22 07:09:01 -0800

    last updates

--- client/src/contexts/MainContext.js
+++ client/src/contexts/MainContext.js
@@ -3,19 +3,19 @@ import { getUserFromLocalStorage } from '@/utils/localStorage';
 import React, { useEffect, useState } from 'react';
 import { createContext, useContext } from 'react';
 import { useSelector } from 'react-redux';
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 const MainContext = createContext();
-const getSocket = () => {
-  const socket = io('http://localhost:8900');
-  return socket;
-};
+// const getSocket = () => {
+//   const socket = io('http://localhost:8900');
+//   return socket;
+// };
 
 export default function MainProvider({ children }) {
   const [socket, setSocket] = useState(null);
   const user = {};
-  useEffect(() => {
-    setSocket(getSocket());
-  }, []);
+  // useEffect(() => {
+  //   setSocket(getSocket());
+  // }, []);
   return (
     <MainContext.Provider
       value={{
