5e110ae6bc50a2f735cb29cd080bc7b1e6c72fa9
ahmed <ahmedp76b@gmail.com>
2023-11-22 22:39:54 -0800

    last updates

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,95 +1,100 @@
-import { List, Title } from '@/components';
-import { domainUrl } from '@/utils/axios';
-import Link from 'next/link';
-import React from 'react';
-async function getOffers() {
-  const res = await fetch(`${domainUrl}/api/v1/listing?offer=true&limit=5`, {
-    next: {
-      revalidate: 10,
-    },
-  });
-  return res.json();
-}
-async function getRents() {
-  const res = await fetch(`${domainUrl}/api/v1/listing?type=rent&limit=5`, {
-    next: {
-      revalidate: 10,
-    },
-  });
-  return res.json();
-}
-async function getSales() {
-  const res = await fetch(`${domainUrl}/api/v1/listing?type=sale&limit=5`, {
-    next: {
-      revalidate: 10,
-    },
-  });
-  return res.json();
-}
-export default async function page() {
-  const [offers, rents, sales] = await Promise.all([
-    getOffers(),
-    getRents(),
-    getSales(),
-  ]);
+// import { List, Title } from '@/components';
+// import { domainUrl } from '@/utils/axios';
+// import Link from 'next/link';
+// import React from 'react';
+// async function getOffers() {
+//   const res = await fetch(`${domainUrl}/api/v1/listing?offer=true&limit=5`, {
+//     next: {
+//       revalidate: 10,
+//     },
+//   });
+//   return res.json();
+// }
+// async function getRents() {
+//   const res = await fetch(`${domainUrl}/api/v1/listing?type=rent&limit=5`, {
+//     next: {
+//       revalidate: 10,
+//     },
+//   });
+//   return res.json();
+// }
+// async function getSales() {
+//   const res = await fetch(`${domainUrl}/api/v1/listing?type=sale&limit=5`, {
+//     next: {
+//       revalidate: 10,
+//     },
+//   });
+//   return res.json();
+// }
+// export default async function page() {
+//   const [offers, rents, sales] = await Promise.all([
+//     getOffers(),
+//     getRents(),
+//     getSales(),
+//   ]);
 
-  return (
-    <div className='align-element mt-10 sm:mt-24'>
-      <div className='max-w-2xl'>
-        <Title title='We provide you with the most important statistics and investment ratios in a simplified manner ' />
-        <p className='paragraph'>
-          Ahmed Estate will help you find your home fast, easy and comfortable.
-          Our expert support are always available.
-        </p>
-      </div>
-      <div className='mt-4 sm:mt-6'>
-        <h3 className='text-lg sm:text-xl text-neutral font-bold'>
-          Recent Offers
-        </h3>
-        <div className='flex items-center gap-2'>
-          <span className='text-lg'>for more offers:</span>
-          <Link href={'search?offer=true'} className='btn btn-neutral btn-xs'>
-            View More
-          </Link>
-        </div>
-        <div className='mt-3 grid sm:grid-cols-3 gap-3'>
-          {offers?.listings?.map((list) => (
-            <List key={list._id} list={list} />
-          ))}
-        </div>
-      </div>
-      <div className='mt-4 sm:mt-6'>
-        <h3 className='text-lg sm:text-xl text-neutral font-bold'>
-          Recent places for rents
-        </h3>
-        <div className='flex items-center gap-2'>
-          <span className='text-lg'>show more places for rent:</span>
-          <Link href={'search?type=rent'} className='btn btn-neutral btn-xs'>
-            View More
-          </Link>
-        </div>
-        <div className='mt-3 grid sm:grid-cols-3 gap-3'>
-          {rents?.listings?.map((list) => (
-            <List key={list._id} list={list} />
-          ))}
-        </div>
-      </div>
-      <div className='mt-4 sm:mt-6'>
-        <h3 className='text-lg sm:text-xl text-neutral font-bold'>
-          Sales places for rents
-        </h3>
-        <div className='flex items-center gap-2'>
-          <span className='text-lg'>show more places for sale:</span>
-          <Link href={'search?type=rent'} className='btn btn-neutral btn-xs'>
-            View More
-          </Link>
-        </div>
-        <div className='mt-3 grid sm:grid-cols-3 gap-3'>
-          {sales?.listings?.map((list) => (
-            <List key={list._id} list={list} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+//   return (
+//     <div className='align-element mt-10 sm:mt-24'>
+//       <div className='max-w-2xl'>
+//         <Title title='We provide you with the most important statistics and investment ratios in a simplified manner ' />
+//         <p className='paragraph'>
+//           Ahmed Estate will help you find your home fast, easy and comfortable.
+//           Our expert support are always available.
+//         </p>
+//       </div>
+//       <div className='mt-4 sm:mt-6'>
+//         <h3 className='text-lg sm:text-xl text-neutral font-bold'>
+//           Recent Offers
+//         </h3>
+//         <div className='flex items-center gap-2'>
+//           <span className='text-lg'>for more offers:</span>
+//           <Link href={'search?offer=true'} className='btn btn-neutral btn-xs'>
+//             View More
+//           </Link>
+//         </div>
+//         <div className='mt-3 grid sm:grid-cols-3 gap-3'>
+//           {offers?.listings?.map((list) => (
+//             <List key={list._id} list={list} />
+//           ))}
+//         </div>
+//       </div>
+//       <div className='mt-4 sm:mt-6'>
+//         <h3 className='text-lg sm:text-xl text-neutral font-bold'>
+//           Recent places for rents
+//         </h3>
+//         <div className='flex items-center gap-2'>
+//           <span className='text-lg'>show more places for rent:</span>
+//           <Link href={'search?type=rent'} className='btn btn-neutral btn-xs'>
+//             View More
+//           </Link>
+//         </div>
+//         <div className='mt-3 grid sm:grid-cols-3 gap-3'>
+//           {rents?.listings?.map((list) => (
+//             <List key={list._id} list={list} />
+//           ))}
+//         </div>
+//       </div>
+//       <div className='mt-4 sm:mt-6'>
+//         <h3 className='text-lg sm:text-xl text-neutral font-bold'>
+//           Sales places for rents
+//         </h3>
+//         <div className='flex items-center gap-2'>
+//           <span className='text-lg'>show more places for sale:</span>
+//           <Link href={'search?type=rent'} className='btn btn-neutral btn-xs'>
+//             View More
+//           </Link>
+//         </div>
+//         <div className='mt-3 grid sm:grid-cols-3 gap-3'>
+//           {sales?.listings?.map((list) => (
+//             <List key={list._id} list={list} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+import React from 'react';
+
+export default function page() {
+  return <div>page</div>;
 }
--- client/src/components/UserChats.jsx
+++ client/src/components/UserChats.jsx
@@ -18,66 +18,66 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function UserChats() {
-  // const { socket } = useMainContext();
+  const { socket } = useMainContext();
 
-  // const { user, chats } = useSelector((store) => store.user);
-  // // const [chats, setChats] = useState([]);
-  // const router = useRouter();
-  // const dispatch = useDispatch();
-  // const onMessageReceived = (message) => {
-  //   if (message?.attachments?.length > 0) {
-  //     console.log(message);
-  //     dispatch(
-  //       updateLastMessage({
-  //         chatToUpdateId: message.chatId,
-  //         message: message.attachments,
-  //         keyMsg: 'attachments',
-  //       })
-  //     );
-  //   } else {
-  //     dispatch(
-  //       updateLastMessage({
-  //         chatToUpdateId: message.chatId,
-  //         message: message.text,
-  //         keyMsg: 'text',
-  //       })
-  //     );
-  //   }
-  // };
+  const { user, chats } = useSelector((store) => store.user);
+  // const [chats, setChats] = useState([]);
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const onMessageReceived = (message) => {
+    if (message?.attachments?.length > 0) {
+      console.log(message);
+      dispatch(
+        updateLastMessage({
+          chatToUpdateId: message.chatId,
+          message: message.attachments,
+          keyMsg: 'attachments',
+        })
+      );
+    } else {
+      dispatch(
+        updateLastMessage({
+          chatToUpdateId: message.chatId,
+          message: message.text,
+          keyMsg: 'text',
+        })
+      );
+    }
+  };
 
-  // const onNewChat = (chat) => {
-  //   dispatch(newChat(chat));
-  //   console.log(chat);
-  // };
+  const onNewChat = (chat) => {
+    dispatch(newChat(chat));
+    console.log(chat);
+  };
 
-  // const getChats = async () => {
-  //   try {
-  //     const { data } = await customFetch('/chat');
-  //     dispatch(updateChats(data.chats));
-  //   } catch (error) {
-  //     checkForUnauthorizedResponse({ error, dispatch, router });
-  //   }
-  // };
-  // useEffect(() => {
-  //   getChats();
-  // }, []);
-  // useEffect(() => {
-  //   if (!socket) return;
-  //   socket.emit('addUser', user.userId);
-  //   socket.on('getMessage', onMessageReceived);
-  //   socket.on('newChat', onNewChat);
-  //   return () => {
-  //     socket.off('getMessage');
-  //     socket.off('newChat');
-  //   };
-  // }, [socket]);
+  const getChats = async () => {
+    try {
+      const { data } = await customFetch('/chat');
+      dispatch(updateChats(data.chats));
+    } catch (error) {
+      checkForUnauthorizedResponse({ error, dispatch, router });
+    }
+  };
+  useEffect(() => {
+    getChats();
+  }, []);
+  useEffect(() => {
+    if (!socket) return;
+    socket.emit('addUser', user.userId);
+    socket.on('getMessage', onMessageReceived);
+    socket.on('newChat', onNewChat);
+    return () => {
+      socket.off('getMessage');
+      socket.off('newChat');
+    };
+  }, [socket]);
 
   return (
     <div className='col-span-1 rounded-lg border-2 h-[calc(100vh-64px)]  overflow-y-auto'>
       <h3 className='font-semibold bg-base-300 px-2 py-5 border-b-2 border-gray-300'>
         Inbox
       </h3>
-      {/* <div className='flex flex-col gap-3  overflow-y-auto'>
+      <div className='flex flex-col gap-3  overflow-y-auto'>
         {chats.map((chat) => {
           const rightUser = chat.members.find(
             (member) => member._id !== user.userId
@@ -123,7 +123,7 @@ export default function UserChats() {
             </div>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 }
